352192407fae17613152dfbd3ca1c2c4558a78ce
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2022-01-14 23:16:30 +0100

    Webserver can now handle multiple receivers and set timeshift

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -4,8 +4,6 @@
 
 String Argument_Name, Clients_Response;
 
-//init external vars
-int timeshift = 0;
 ESP8266WebServer server(80);
 
 void HandleClient();
@@ -42,6 +40,24 @@ void HandleClient() {
       webpage += "</select>";
       webpage += "<input type='submit' value='Apply Change'>";
      webpage += "</form>";
+     webpage += "<form action='http://"+IPaddress+"' method='POST'>";
+      webpage += "<select name='clock2'>";
+        webpage += "<option value='0'>UTC 0</option>";
+        webpage += "<option value='3600'>UTC +1</option>";
+        webpage += "<option value='10800'>UTC +3</option>";
+        webpage += "<option value='-18000'>UTC -5</option>";
+      webpage += "</select>";
+      webpage += "<input type='submit' value='Apply Change'>";
+     webpage += "</form>";
+     webpage += "<form action='http://"+IPaddress+"' method='POST'>";
+      webpage += "<select name='clock3'>";
+        webpage += "<option value='0'>UTC 0</option>";
+        webpage += "<option value='3600'>UTC +1</option>";
+        webpage += "<option value='10800'>UTC +3</option>";
+        webpage += "<option value='-18000'>UTC -5</option>";
+      webpage += "</select>";
+      webpage += "<input type='submit' value='Apply Change'>";
+     webpage += "</form>";
    webpage += "</body>";
   webpage += "</html>";
   server.send(200, "text/html", webpage); // Send a response to the client asking for input
@@ -50,11 +66,26 @@ void HandleClient() {
   if (server.args() > 0 ) { 
     for ( uint8_t i = 0; i < server.args(); i++ ) {
       Argument_Name = server.argName(i);
-      if (server.argName(i) == "clock1") {
+      if (Argument_Name == "clock1") {
         Serial.print(" Input received was: ");
         Serial.println(server.arg(i));
         Clients_Response = server.arg(i);
-        timeshift = server.arg(i).toInt();
+        int timeshift = Clients_Response.toInt();
+        receivers[0].timeshift = timeshift;
+      }
+      if (Argument_Name == "clock2") {
+        Serial.print(" Input received was: ");
+        Serial.println(server.arg(i));
+        Clients_Response = server.arg(i);
+        int timeshift = Clients_Response.toInt();
+        receivers[2].timeshift = timeshift;
+      }
+      if (Argument_Name == "clock3") {
+        Serial.print(" Input received was: ");
+        Serial.println(server.arg(i));
+        Clients_Response = server.arg(i);
+        int timeshift = Clients_Response.toInt();
+        receivers[3].timeshift = timeshift;
       }
     }
   }
